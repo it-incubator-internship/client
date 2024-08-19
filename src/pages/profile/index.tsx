3718c13b2a0cfc1b9d4f8f155ea5e8c4c1e98650
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-08-19 18:45:51 +0400

    master updated ui-kit lib

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,10 +1,14 @@
 import Table from '@/components/Table/Table'
+import { Card } from '@robur_/ui-kit'
 
 export default function Profile() {
   return (
     <div>
       Profile page
       <Table />
+      <Card>
+        <h1>safdsdfsdf</h1>
+      </Card>
     </div>
   )
 }
