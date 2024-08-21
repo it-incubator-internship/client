73662644b53e61dfcaab8097c5220641b6bd7b72
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-08-21 16:50:36 +0400

    [IN-35] add layout

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,14 +1,30 @@
-import Table from '@/components/Table/Table'
-import { Card } from '@robur_/ui-kit'
+import { getLayout } from '@/components/Layout/Layout'
+import { TabContent, Tabs } from '@robur_/ui-kit'
 
-export default function Profile() {
+function Profile() {
   return (
-    <div>
-      Profile page
-      <Table />
-      <Card>
-        <h1>safdsdfsdf</h1>
-      </Card>
+    <div className={'container'}>
+      <Tabs
+        defaultValue={'information'}
+        fullWidth
+        tabs={[
+          { title: 'General information', value: 'information' },
+          { disabled: true, title: 'Devices', value: 'devices' },
+          { disabled: true, title: 'Account Management', value: 'account' },
+          { disabled: true, title: 'My payments', value: 'payments' },
+        ]}
+      >
+        <>
+          <TabContent value={'information'}>Content about General information</TabContent>
+          <TabContent value={'devices'}>Content about Devices</TabContent>
+          <TabContent value={'account'}>Content about Account Management</TabContent>
+          <TabContent value={'payments'}>Content about My payments</TabContent>
+        </>
+      </Tabs>
     </div>
   )
 }
+
+Profile.getLayout = getLayout
+
+export default Profile
