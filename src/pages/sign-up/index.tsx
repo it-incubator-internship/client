598a2083cf76040fe0e2444b186f0097da0365e8
Shevchenko Andrey <demorest49de@gmail.com>
2024-08-22 21:39:40 +0300

    IN-31: sign up form changed, styles changed, package.json updated;         modified:   package.json         modified:   src/pages/sign-up/Signup.module.scss         modified:   src/pages/sign-up/index.tsx

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,18 +1,29 @@
 import clsx from "clsx";
-import {Card} from '@robur_/ui-kit'
+import {
+    Card,
+    GithubSvgrepoCom31,
+    GoogleSvgrepoCom1,
+    ControlledInput,
+} from '@robur_/ui-kit'
 import s from './Signup.module.scss'
+import Link from "next/link";
 
 
 export default function SignUp() {
     return (
         <div className={clsx(s.SignUpContainer)}>
-            <Card className={clsx("flex", "justify-center", s.SignUpCard)}>
+            <Card className={clsx(s.SignUpCard)}>
                 <h1 className={s.SignUpCardTitle}>Sign Up</h1>
                 <div className={s.SignUpCloudAuth}>
-                    <GithubSvgrepoCom31/>
-                    <GoogleSvgrepoCom1/>
+                    <Link className={s.SignUpCloudAuthLink} href={'https://www.google.com'}>
+                        <GoogleSvgrepoCom1/>
+                    </Link>
+                    <Link className={s.SignUpCloudAuthLink} href={'https://www.github.com'}>
+                        <GithubSvgrepoCom31 />
+                    </Link>
                 </div>
-                <form>
+                <form className={s.SignUpForm}>
+                    {/*<ControlledInput control={}/>*/}
                 </form>
             </Card>
         </div>
