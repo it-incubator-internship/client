4c73bb898ba821dd9acfe14179f16501f1bd7ae0
Shevchenko Andrey <demorest49de@gmail.com>
2024-08-22 20:21:07 +0300

    IN-31: sign up form created, styles added

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,3 +1,20 @@
+import clsx from "clsx";
+import {Card} from '@robur_/ui-kit'
+import s from './Signup.module.scss'
+
+
 export default function SignUp() {
-  return <div>Sign up page</div>
+    return (
+        <div className={clsx(s.SignUpContainer)}>
+            <Card className={clsx("flex", "justify-center", s.SignUpCard)}>
+                <h1 className={s.SignUpCardTitle}>Sign Up</h1>
+                <div className={s.SignUpCloudAuth}>
+                    <GithubSvgrepoCom31/>
+                    <GoogleSvgrepoCom1/>
+                </div>
+                <form>
+                </form>
+            </Card>
+        </div>
+    )
 }
