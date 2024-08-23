13d95a77e3ddc05cb4755e5984fffd24d8db41f3
Shevchenko Andrey <demorest49de@gmail.com>
2024-08-23 09:54:01 +0300

    IN-31: sign up form changed, styles changed,         modified:   src/pages/sign-up/Signup.module.scss         modified:   src/pages/sign-up/index.tsx

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,31 +1,38 @@
-import clsx from "clsx";
-import {
-    Card,
-    GithubSvgrepoCom31,
-    GoogleSvgrepoCom1,
-    ControlledInput,
-} from '@robur_/ui-kit'
+import clsx from 'clsx'
+import { Card, ControlledInput, GithubSvgrepoCom31, GoogleSvgrepoCom1, Label } from '@robur_/ui-kit'
 import s from './Signup.module.scss'
-import Link from "next/link";
-
+import Link from 'next/link'
+import { useForm } from 'react-hook-form'
 
 export default function SignUp() {
-    return (
-        <div className={clsx(s.SignUpContainer)}>
-            <Card className={clsx(s.SignUpCard)}>
-                <h1 className={s.SignUpCardTitle}>Sign Up</h1>
-                <div className={s.SignUpCloudAuth}>
-                    <Link className={s.SignUpCloudAuthLink} href={'https://www.google.com'}>
-                        <GoogleSvgrepoCom1/>
-                    </Link>
-                    <Link className={s.SignUpCloudAuthLink} href={'https://www.github.com'}>
-                        <GithubSvgrepoCom31 />
-                    </Link>
-                </div>
-                <form className={s.SignUpForm}>
-                    {/*<ControlledInput control={}/>*/}
-                </form>
-            </Card>
+  const { register, handleSubmit, control } = useForm()
+
+  const submitForm = handleSubmit(data => {
+    console.log(data)
+  })
+
+  return (
+    <div className={clsx(s.SignUpContainer)}>
+      <Card className={clsx(s.SignUpCard, s.BorderBack)}>
+        <h1 className={s.SignUpCardTitle}>Sign Up</h1>
+        <div className={s.SignUpCloudAuth}>
+          <Link className={s.SignUpCloudAuthLink} href={'https://www.google.com'} target={'_blank'}>
+            <GoogleSvgrepoCom1 />
+          </Link>
+          <Link className={s.SignUpCloudAuthLink} href={'https://www.github.com'} target={'_blank'}>
+            <GithubSvgrepoCom31 />
+          </Link>
         </div>
-    )
+        <form className={s.SignUpForm}>
+          <Label label={'Username'} className={s.SignUpFormLabel}>
+            <ControlledInput
+              control={control}
+              {...register('username')}
+              className={clsx(s.SignUpFormInput, s.BorderBack)}
+            />
+          </Label>
+        </form>
+      </Card>
+    </div>
+  )
 }
