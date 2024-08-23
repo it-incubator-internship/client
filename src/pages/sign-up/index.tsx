658bc1e13a8b090ea7aefc094fe856bedde6bb63
Shevchenko Andrey <demorest49de@gmail.com>
2024-08-23 11:18:49 +0300

    IN-31: sign up form changed, styles changed,         modified:   src/pages/sign-up/Signup.module.scss         modified:   src/pages/sign-up/index.tsx

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,5 +1,12 @@
 import clsx from 'clsx'
-import { Card, ControlledInput, GithubSvgrepoCom31, GoogleSvgrepoCom1, Label } from '@robur_/ui-kit'
+import {
+  Card,
+  Checkbox,
+  ControlledInput,
+  GithubSvgrepoCom31,
+  GoogleSvgrepoCom1,
+  Label,
+} from '@robur_/ui-kit'
 import s from './Signup.module.scss'
 import Link from 'next/link'
 import { useForm } from 'react-hook-form'
@@ -26,12 +33,51 @@ export default function SignUp() {
         <form className={s.SignUpForm}>
           <Label label={'Username'} className={s.SignUpFormLabel}>
             <ControlledInput
+              type="text"
               control={control}
+              rules={{ required: true }}
               {...register('username')}
               className={clsx(s.SignUpFormInput, s.BorderBack)}
             />
           </Label>
+
+          <Label label={'Email'} className={s.SignUpFormLabel}>
+            <ControlledInput
+              type="email"
+              control={control}
+              rules={{ required: true }}
+              {...register('Email')}
+              className={clsx(s.SignUpFormInput, s.BorderBack)}
+            />
+          </Label>
+
+          <Label label={'Password'} className={s.SignUpFormLabel}>
+            <ControlledInput
+              type={'password'}
+              control={control}
+              rules={{ required: true }}
+              {...register('Password')}
+              className={clsx(s.SignUpFormInput, s.BorderBack)}
+            />
+          </Label>
+
+          <Label label={'Password confirmation'} className={s.SignUpFormLabel}>
+            <ControlledInput
+              type={'password'}
+              shouldUnregister={true}
+              control={control}
+              rules={{ required: true }}
+              {...register('Password confirmation')}
+              className={clsx(s.SignUpFormInput, s.BorderBack)}
+            />
+          </Label>
         </form>
+        <Checkbox
+          checked={true}
+          id={'SignUpAgreementBlock'}
+          labelText={'I agree to the '}
+          className={s.SignUpAgreementCheckbox}
+        />
       </Card>
     </div>
   )
