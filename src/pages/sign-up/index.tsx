120426c50f9a2998ac0b1c9693168fdd65469a6d
Shevchenko Andrey <demorest49de@gmail.com>
2024-08-24 09:50:49 +0300

    IN-31: Changes index.tsx package.json pnpm-lock.yaml

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,84 +1,92 @@
-import clsx from 'clsx'
+import clsx from "clsx";
 import {
   Card,
-  Checkbox,
-  ControlledInput,
+  FormCheckbox,
+  FormInput,
   GithubSvgrepoCom31,
   GoogleSvgrepoCom1,
-  Label,
-} from '@robur_/ui-kit'
-import s from './Signup.module.scss'
-import Link from 'next/link'
-import { useForm } from 'react-hook-form'
+  Label
+} from "@robur_/ui-kit";
+import s from "./Signup.module.scss";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 export default function SignUp() {
-  const { register, handleSubmit, control } = useForm()
+  const {
+    register
+    , handleSubmit,
+    control
+  }
+    = useForm();
 
   const submitForm = handleSubmit(data => {
-    console.log(data)
-  })
+    console.log(data);
+  });
 
   return (
     <div className={clsx(s.SignUpContainer)}>
       <Card className={clsx(s.SignUpCard, s.BorderBack)}>
         <h1 className={s.SignUpCardTitle}>Sign Up</h1>
         <div className={s.SignUpCloudAuth}>
-          <Link className={s.SignUpCloudAuthLink} href={'https://www.google.com'} target={'_blank'}>
+          <Link className={s.SignUpCloudAuthLink} href={"https://www.google.com"} target={"_blank"}>
             <GoogleSvgrepoCom1 />
           </Link>
-          <Link className={s.SignUpCloudAuthLink} href={'https://www.github.com'} target={'_blank'}>
+          <Link className={s.SignUpCloudAuthLink} href={"https://www.github.com"} target={"_blank"}>
             <GithubSvgrepoCom31 />
           </Link>
         </div>
         <form className={s.SignUpForm}>
-          <Label label={'Username'} className={s.SignUpFormLabel}>
-            <ControlledInput
+          <Label label={"Username"} className={s.SignUpFormLabel}>
+            <FormInput
               type="text"
               control={control}
               rules={{ required: true }}
-              {...register('username')}
               className={clsx(s.SignUpFormInput, s.BorderBack)}
+              name={"username"}
             />
           </Label>
 
-          <Label label={'Email'} className={s.SignUpFormLabel}>
-            <ControlledInput
+          <Label label={"Email"} className={s.SignUpFormLabel}>
+            <FormInput
               type="email"
               control={control}
               rules={{ required: true }}
-              {...register('Email')}
+              name={"email"}
               className={clsx(s.SignUpFormInput, s.BorderBack)}
             />
           </Label>
 
-          <Label label={'Password'} className={s.SignUpFormLabel}>
-            <ControlledInput
-              type={'password'}
+          <Label label={"Password"} className={s.SignUpFormLabel}>
+            <FormInput
+              type={"password"}
               control={control}
               rules={{ required: true }}
-              {...register('Password')}
+              name={"password"}
               className={clsx(s.SignUpFormInput, s.BorderBack)}
             />
           </Label>
 
-          <Label label={'Password confirmation'} className={s.SignUpFormLabel}>
-            <ControlledInput
-              type={'password'}
+          <Label label={"Password confirmation"} className={s.SignUpFormLabel}>
+            <FormInput
+              type={"password"}
               shouldUnregister={true}
               control={control}
               rules={{ required: true }}
-              {...register('Password confirmation')}
+              name={"password"}
               className={clsx(s.SignUpFormInput, s.BorderBack)}
             />
           </Label>
         </form>
-        <Checkbox
+        <
+          FormCheckbox
+          control={control}
+          name={"SignUpAgreement"}
           checked={true}
-          id={'SignUpAgreementBlock'}
-          labelText={'I agree to the '}
+          id={"SignUpAgreementCheckbox"}
+          labelText={"I agree to the "}
           className={s.SignUpAgreementCheckbox}
         />
       </Card>
     </div>
-  )
+  );
 }
