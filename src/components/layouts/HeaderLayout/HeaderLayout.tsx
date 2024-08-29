5c7f6e8b19bf9ae299e4d2c93d8f8e27a42fddfd
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-08-29 18:53:40 +0400

    [IN-60] add Header component

--- src/components/layouts/HeaderLayout/HeaderLayout.tsx
+++ src/components/layouts/HeaderLayout/HeaderLayout.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren, ReactElement } from 'react'
 
+import { Header } from '@/components/Header/Header'
 import { NextPage } from 'next'
 
 import s from './HeaderLayout.module.scss'
@@ -7,7 +8,7 @@ import s from './HeaderLayout.module.scss'
 const HeaderLayout: NextPage<PropsWithChildren> = ({ children }) => {
   return (
     <main className={s.main}>
-      <header>HEADER</header>
+      <Header />
       <div className={s.container}>{children}</div>
     </main>
   )
