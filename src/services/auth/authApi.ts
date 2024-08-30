7a949ec8f55203d7af4bb3897fe6ba3e7019ace6
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-08-30 20:09:16 +0400

    [IN-43] add setting access token

--- src/services/auth/authApi.ts
+++ src/services/auth/authApi.ts
@@ -5,7 +5,22 @@ import { inctagramApi } from '../inctagramApi'
 const authApi = inctagramApi.injectEndpoints({
   endpoints: builder => ({
     login: builder.mutation<LoginResponse, LoginArgs>({
-      invalidatesTags: ['Me'],
+      async onQueryStarted(
+        // 1 параметр: QueryArg - аргументы, которые приходят в query
+        _,
+        // 2 параметр: MutationLifecycleApi - dispatch, queryFulfilled, getState и пр.
+        // queryFulfilled - это промис, возвращаемый RTK Query, который разрешается,
+        // когда запрос успешно завершен
+        { queryFulfilled }
+      ) {
+        const { data } = await queryFulfilled
+
+        if (!data) {
+          return
+        }
+
+        localStorage.setItem('accessToken', data.accessToken)
+      },
       query: ({ email, password }) => ({
         body: { email, password },
         credentials: 'same-origin',
