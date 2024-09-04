82c7865e6ecab0d1ec7e9f32f7f51e0c74883e26
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-09-04 17:35:21 +0400

    [master] add next-redux-wrapper

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,12 +1,20 @@
 // eslint-disable-next-line import/no-unresolved
 import { configureStore } from '@reduxjs/toolkit'
+import { createWrapper } from 'next-redux-wrapper'
 
 import { inctagramApi } from './inctagramApi'
 
-export const store = configureStore({
-  devTools: true,
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(inctagramApi.middleware),
-  reducer: {
-    [inctagramApi.reducerPath]: inctagramApi.reducer,
-  },
-})
+// create a makeStore function
+const makeStore = () =>
+  configureStore({
+    devTools: true,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(inctagramApi.middleware),
+    reducer: {
+      [inctagramApi.reducerPath]: inctagramApi.reducer,
+    },
+  })
+
+export type RootAppState = ReturnType<typeof makeStore>
+
+// export an assembled wrapper
+export const wrapper = createWrapper<RootAppState>(makeStore, { debug: false })
