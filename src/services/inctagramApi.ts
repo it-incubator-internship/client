ef47ede8e0fc622c987d5be798c635a389b7d89c
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-09-05 11:34:50 +0400

    [IN-47] upd

--- src/services/inctagramApi.ts
+++ src/services/inctagramApi.ts
@@ -1,5 +1,4 @@
-import { fetchBaseQuery } from '@reduxjs/toolkit/query'
-import { createApi } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NEXT_PUBLIC_BASE_API_URL,
