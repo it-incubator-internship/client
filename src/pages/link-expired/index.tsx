5e6aeac54848b16d0bc71a61ff4d0cb038c386ed
Shevchenko Andrey <demorest49de@gmail.com>
2024-09-10 10:25:42 +0300

    Changes email-confirmed.png index.tsx link-expired.module.scss

--- src/pages/link-expired/index.tsx
+++ src/pages/link-expired/index.tsx
@@ -1,25 +1,28 @@
-import { Button } from '@robur_/ui-kit'
-import Image from 'next/image'
+import { Button } from "@robur_/ui-kit";
+import src from "../../../public/TimeManagement.png";
+import Image from "next/image";
 
-import s from './link-expired.module.scss'
+import s from "./link-expired.module.scss";
 
 export default function LinkExpired() {
   const handleOnClick = () => {
-    alert('The link was sent again')
-  }
-
+    alert("The link was sent again");
+  };
+  console.log(src);
   return (
-    <div className={s.outerWrapper}>
-      <div className={s.innerWrapper}>
-        <h1 className={s.title}>Email verification link expired</h1>
-        <p className={s.text}>
-          Looks like the verification link has expired. Not to worry, we can send the link again
-        </p>
-        <Button fullWidth onClick={handleOnClick}>
-          Resend link
-        </Button>
+    <div className={s.container}>
+      <div className={s.outerWrapper}>
+        <div className={s.innerWrapper}>
+          <h1 className={s.title}>Email verification link expired</h1>
+          <p className={s.text}>
+            Looks like the verification link has expired. Not to worry, we can send the link again
+          </p>
+          <Button fullWidth onClick={handleOnClick}>
+            Resend verification link
+          </Button>
+        </div>
+        <Image src={src} alt="email-confirmed" className={s.image} />
       </div>
-      <Image alt={'link expired'} src={'/TimeManagement.png'} />
     </div>
-  )
+  );
 }
