dc526508c11f960042ed936fa4fd33d61c387dc3
Rentoshiro <r.renat1998@gmail.com>
2024-09-16 23:35:44 +0300

    [IN-45] added modal for logout

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useTranslation } from '@/hooks/useTranslation'
 import { useLogoutMutation, useMeQuery } from '@/services/auth/authApi'
 import {
@@ -10,6 +11,8 @@ import {
   Search,
   Sidebar,
   TrendingUpOutline,
+  Modal,
+  Button,
 } from '@robur_/ui-kit'
 
 import { SidebarItem } from './SidebarItem/SidebarItem'
@@ -22,13 +25,26 @@ export const Navbar = ({ className }: Props) => {
   const { data } = useMeQuery()
 
   const [logout] = useLogoutMutation()
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  function handleModalOpened() {
+    setModalIsOpen(true)
+  }
+
+  function handleModalClosed() {
+    setModalIsOpen(false)
+  }
+
+  function handleModalApproved() {
+    logout()
+  }
 
   return (
     <nav className={className}>
       <Sidebar>
         <SidebarItem Icon={HomeOutline} href={'/'} item={t.nav.home} />
         <SidebarItem Icon={PlusSquareOutline} href={'/'} item={t.nav.create} />
-        <SidebarItem Icon={Person} href={`/profile/${data?.userId}/edit`} item={t.nav.myProfile} />
+        <SidebarItem Icon={Person} href={`/profile/${data?.userId}`} item={t.nav.myProfile} />
         <SidebarItem Icon={MessageCircleOutline} href={'/'} item={t.nav.messenger} />
         <SidebarItem Icon={Search} href={'/'} item={t.nav.search} />
         <div style={{ marginTop: '60px' }}>
@@ -36,11 +52,23 @@ export const Navbar = ({ className }: Props) => {
           <SidebarItem Icon={BookmarkOutline} href={'/'} item={t.nav.favorites} />
         </div>
         <div style={{ marginTop: '150px' }}>
-          <div onClick={() => logout()}>
+          <div onClick={() => handleModalOpened()}>
             <SidebarItem Icon={LogOut} href={'/'} item={t.nav.logout} />
           </div>
         </div>
       </Sidebar>
+      <Modal
+        buttonRejectionTitle={'No'}
+        buttonTitle={'Yes'}
+        onClose={handleModalClosed}
+        onCloseWithApproval={handleModalApproved}
+        open
+        title={'Log out'}
+        withConfirmation
+        open={modalIsOpen}
+      >
+        <p>{`Are you really want to log out of your account ${data?.userName}?`}</p>
+      </Modal>
     </nav>
   )
 }
