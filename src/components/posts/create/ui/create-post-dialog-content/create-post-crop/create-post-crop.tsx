cf50230c824822811c9abe58d5e4b236ca5573a8
Shevchenko Andrey <demorest49de@gmail.com>
2024-10-20 23:24:13 +0300

    Changes create-post-crop.module.scss create-post-crop.tsx create-post-dialog.module.scss create-post-dialog-header.module.scss package.json

--- src/components/posts/create/ui/create-post-dialog-content/create-post-crop/create-post-crop.tsx
+++ src/components/posts/create/ui/create-post-dialog-content/create-post-crop/create-post-crop.tsx
@@ -1,9 +1,34 @@
+import { useRef, useState } from 'react'
+
 import { useAppSelector } from '@/services/store'
+import Image from 'next/image'
+
+import s from './create-post-crop.module.scss'
 
 export const CreatePostCrop = () => {
   const images = useAppSelector(state => state.createPost.images)
 
+  const cropperRef = useRef<HTMLImageElement>(null)
+  const [imageSrc, setImageSrc] = useState<null | string>(null)
+
   console.log('images', images)
 
-  return <div>CreatePostCrop</div>
+  const imgStyle = {
+    height: 503,
+    width: 490,
+  }
+
+  return (
+    <div className={s['create-post-cropp-wrapper']}>
+      {images.length && (
+        <Image
+          alt={'image to crop'}
+          className={s['create-post-cropp-image']}
+          height={imgStyle.height}
+          src={images[0].img}
+          width={imgStyle.width}
+        />
+      )}
+    </div>
+  )
 }
