783d7a2c72a96e5e632c6cec81079f04dc58da46
Shevchenko Andrey <demorest49de@gmail.com>
2024-10-21 10:56:22 +0300

    Changes create-post-crop.module.scss create-post-crop.tsx create-post-dialog.module.scss

--- src/components/posts/create/ui/create-post-dialog-content/create-post-crop/create-post-crop.tsx
+++ src/components/posts/create/ui/create-post-dialog-content/create-post-crop/create-post-crop.tsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from 'react'
 
 import { useAppSelector } from '@/services/store'
+import { Button, ExpandOutline, ImageOutline, MaximizeOutline } from '@robur_/ui-kit'
+import clsx from 'clsx'
 import Image from 'next/image'
 
 import s from './create-post-crop.module.scss'
@@ -29,6 +31,31 @@ export const CreatePostCrop = () => {
           width={imgStyle.width}
         />
       )}
+      <div className={s['create-post-cropp-action-buttons']}>
+        <div className={s['create-post-cropp-btns-block']}>
+          <Button
+            className={clsx(s['create-post-cropp-btn'], s['create-post-cropp-btn-fullscreen'])}
+            onClick={() => {}}
+            variant={'secondary'}
+          >
+            <ExpandOutline />
+          </Button>
+          <Button
+            className={clsx(s['create-post-cropp-btn'], s['create-post-cropp-btn-zoom'])}
+            onClick={() => {}}
+            variant={'secondary'}
+          >
+            <MaximizeOutline />
+          </Button>
+        </div>
+        <Button
+          className={clsx(s['create-post-cropp-btn'], s['create-post-cropp-btn-add-change-photo'])}
+          onClick={() => {}}
+          variant={'secondary'}
+        >
+          <ImageOutline />
+        </Button>
+      </div>
     </div>
   )
 }
