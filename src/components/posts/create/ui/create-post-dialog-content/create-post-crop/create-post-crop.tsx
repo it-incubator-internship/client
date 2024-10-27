0d3a0bb16606bfc0681c9365fb3519a61d5c6be5
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-10-27 16:43:06 +0400

    [IN-135] fixes after merge with master

--- src/components/posts/create/ui/create-post-dialog-content/create-post-crop/create-post-crop.tsx
+++ src/components/posts/create/ui/create-post-dialog-content/create-post-crop/create-post-crop.tsx
@@ -1,4 +1,15 @@
-import { useAppSelector } from '@/services/store'
+import { ChangeEvent, useRef, useState } from 'react'
+
+import {
+  FILE_VALIDATION_CONFIG,
+  deleteImg,
+  setImage,
+  setPage,
+} from '@/components/posts/create/model/create-post-slice'
+import { useTranslation } from '@/hooks/useTranslation'
+import { useAppDispatch, useAppSelector } from '@/services/store'
+import { showErrorToast } from '@/utils/toastConfig'
+import { CloseOutline, ImageOutline, PlusCircleOutline } from '@robur_/ui-kit'
 import Image from 'next/image'
 import { Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
@@ -10,20 +21,7 @@ import 'swiper/scss/navigation'
 // eslint-disable-next-line import/extensions
 import 'swiper/scss/pagination'
 
-import s from './create-post-crop.module.scss'
-import { useState } from 'react'
-import { useAppSelector, useAppDispatch } from '@/services/store'
-import { ChangeEvent, useRef } from 'react'
-import {
-  setImage,
-  deleteImg,
-  setPage,
-  FILE_VALIDATION_CONFIG,
-} from '@/components/posts/create/model/create-post-slice'
-import { ImageOutline, PlusCircleOutline, CloseOutline } from '@robur_/ui-kit'
 import styles from './create-post-crop.module.scss'
-import { showErrorToast } from '@/utils/toastConfig'
-import { useTranslation } from '@/hooks/useTranslation'
 
 export const CreatePostCrop = () => {
   const images = useAppSelector(state => state.createPost.images)
@@ -37,6 +35,7 @@ export const CreatePostCrop = () => {
     if (e.target.files) {
       if (croppedImages.length >= 5) {
         showErrorToast(t.createPost.errorMaxPhotos)
+
         return
       }
 
@@ -44,16 +43,19 @@ export const CreatePostCrop = () => {
 
       if (file.size > FILE_VALIDATION_CONFIG.maxFileSize) {
         showErrorToast(t.createPost.errorFileSize)
+
         return
       }
 
       if (!FILE_VALIDATION_CONFIG.allowedFileTypes.includes(file.type)) {
         showErrorToast(t.createPost.errorFileType)
+
         return
       }
 
       if (file) {
         const fileURL = URL.createObjectURL(file)
+
         dispatch(setImage({ img: fileURL }))
       }
     }
@@ -67,8 +69,15 @@ export const CreatePostCrop = () => {
     setModalOpen(!isModalOpen)
   }
 
+  const handleDeleteImage = (id: number) => {
+    dispatch(deleteImg({ id }))
+    if (croppedImages.length === 1) {
+      dispatch(setPage({ page: 0 }))
+    }
+  }
+
   return (
-    <div className={s.container}>
+    <div className={styles.container}>
       <Swiper
         modules={[Pagination]}
         onSlideChange={() => console.log('slide change')}
@@ -79,10 +88,10 @@ export const CreatePostCrop = () => {
       >
         {images.map(image => {
           return (
-            <SwiperSlide className={s.slide} key={image.id}>
+            <SwiperSlide className={styles.slide} key={image.id}>
               <Image
                 alt={image.id.toString()}
-                className={s.image}
+                className={styles.image}
                 height={504}
                 src={image.img}
                 width={490}
@@ -91,63 +100,41 @@ export const CreatePostCrop = () => {
           )
         })}
       </Swiper>
+      <div className={styles.iconContainer} onClick={toggleModal}>
+        <ImageOutline className={styles.imageIcon} />
+      </div>
+      <input
+        accept={'image/*'}
+        onChange={handleFileUpload}
+        ref={fileInputRef}
+        style={{ display: 'none' }}
+        type={'file'}
+      />
+
+      {isModalOpen && (
+        <div className={styles.customModal}>
+          <div className={styles.modalContent}>
+            <div className={styles.modalImageList}>
+              {croppedImages.map((image, index) => (
+                <div className={styles.imageWrapper} key={image.id}>
+                  <img
+                    alt={`Uploaded image ${index}`}
+                    className={styles.previewImage}
+                    src={image.img}
+                  />
+                  <div className={styles.closeButtonContainer}>
+                    <CloseOutline
+                      className={styles.closeButton}
+                      onClick={() => handleDeleteImage(image.id)}
+                    ></CloseOutline>
+                  </div>
+                </div>
+              ))}
+              <PlusCircleOutline className={styles.plusIconContainer} onClick={openFileUploader} />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
-
-// const handleDeleteImage = (id: number) => {
-//   dispatch(deleteImg({ id }))
-//   if (croppedImages.length === 1) {
-//     dispatch(setPage({ page: 0 }))
-//   }
-// }
-//
-// return (
-//   <div className={styles.container}>
-//     {images.length > 0 && (
-//       <div className={styles.imageContainer}>
-//         <img
-//           src={croppedImages[0].img}
-//           alt="Uploaded"
-//           style={{ width: '100%', height: '504px', objectFit: 'cover' }}
-//         />
-//         <div className={styles.iconContainer} onClick={toggleModal}>
-//           <ImageOutline className={styles.imageIcon} />
-//         </div>
-//       </div>
-//     )}
-//     <input
-//       ref={fileInputRef}
-//       type="file"
-//       accept="image/*"
-//       onChange={handleFileUpload}
-//       style={{ display: 'none' }}
-//     />
-//
-//     {isModalOpen && (
-//       <div className={styles.customModal}>
-//         <div className={styles.modalContent}>
-//           <div className={styles.modalImageList}>
-//             {croppedImages.map((image, index) => (
-//               <div key={image.id} className={styles.imageWrapper}>
-//                 <img
-//                   src={image.img}
-//                   alt={`Uploaded image ${index}`}
-//                   className={styles.previewImage}
-//                 />
-//                 <div className={styles.closeButtonContainer}>
-//                   <CloseOutline
-//                     className={styles.closeButton}
-//                     onClick={() => handleDeleteImage(image.id)}
-//                   ></CloseOutline>
-//                 </div>
-//               </div>
-//             ))}
-//             <PlusCircleOutline className={styles.plusIconContainer} onClick={openFileUploader} />
-//           </div>
-//         </div>
-//       </div>
-//     )}
-//   </div>
-// )
-// }
