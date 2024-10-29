c42ecd58b1247719b05ebbbb897f38c04483c6f5
Shevchenko Andrey <demorest49de@gmail.com>
2024-10-29 20:49:42 +0300

    Changes create-post-crop.module.scss create-post-cropp-options.module.scss create-post-cropp-zoom-button.module.scss index.ts range-slider.module.scss range-slider.tsx ZoomButton.tsx

--- src/components/posts/create/ui/create-post-dialog-content/create-post-crop/range-slider/range-slider.tsx
+++ src/components/posts/create/ui/create-post-dialog-content/create-post-crop/range-slider/range-slider.tsx
@@ -2,39 +2,40 @@ import React, { useState } from 'react'
 
 import * as Slider from '@radix-ui/react-slider'
 
-const styles = {
-  sliderRange: {
-    background: '#0070f3', // Цвет диапазона
-    borderRadius: '3px',
-    height: '100%',
-  } as React.CSSProperties,
+import s from './range-slider.module.scss'
 
-  sliderRoot: {
-    position: 'relative',
-    userSelect: 'none',
-  } as React.CSSProperties,
-
-  sliderThumb: {
-    background: 'white', // Цвет ползунка
-    border: '2px solid #0070f3', // Цвет рамки ползунка
-    borderRadius: '50%',
-    boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
-    cursor: 'grab',
-    height: '20px', // Высота ползунка
-    width: '20px', // Ширина ползунка
-  } as React.CSSProperties,
-
-  sliderTrack: {
-    background: '#e0e0e0', // Цвет трека
-    borderRadius: '3px',
-    height: '6px',
-  } as React.CSSProperties,
-
-  sliderWrapper: {
-    margin: '20px',
-    width: '300px', // Ширина ползунка
-  } as React.CSSProperties,
-}
+// const styles = {
+//   sliderRange: {
+//     background: '#0070f3', // Цвет диапазона
+//     borderRadius: '3px',
+//     height: '100%',
+//   } as React.CSSProperties,
+//
+//   sliderRoot: {
+//     position: 'relative',
+//     userSelect: 'none',
+//   } as React.CSSProperties,
+//
+//   sliderThumb: {
+//     background: 'white', // Цвет ползунка
+//     border: '2px solid #0070f3', // Цвет рамки ползунка
+//     borderRadius: '50%',
+//     boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+//     cursor: 'grab',
+//     height: '20px', // Высота ползунка
+//     width: '20px', // Ширина ползунка
+//   } as React.CSSProperties,
+//
+//   sliderTrack: {
+//     backgroundColor: '#e0e0e0', // Цвет трека
+//     borderRadius: '3px',
+//     height: '6px',
+//   } as React.CSSProperties,
+//
+//   sliderWrapper: {
+//     width: '300px', // Ширина ползунка
+//   } as React.CSSProperties,
+// }
 
 const RangeSlider = () => {
   const [value, setValue] = useState<number[]>([0]) // Начальное значение 50%
@@ -44,20 +45,20 @@ const RangeSlider = () => {
   }
 
   return (
-    <div style={styles.sliderWrapper}>
+    <div className={s.sliderWrapper}>
       <Slider.Root
         aria-label={'Percentage Slider'}
+        className={s.sliderRoot}
         max={100}
         min={0}
         onValueChange={handleValueChange}
         step={1}
-        style={styles.sliderRoot}
         value={value}
       >
-        <Slider.Track style={styles.sliderTrack}>
-          <Slider.Range style={styles.sliderRange} />
+        <Slider.Track className={s.sliderTrack}>
+          <Slider.Range className={s.sliderRange} />
         </Slider.Track>
-        <Slider.Thumb style={styles.sliderThumb} />
+        <Slider.Thumb className={s.sliderThumb} />
       </Slider.Root>
       <div>{value[0]}%</div> {/* Отображение текущего значения */}
     </div>
--- src/components/posts/create/ui/create-post-dialog-content/create-post-crop/zoom-handler/ZoomButton.tsx
+++ src/components/posts/create/ui/create-post-dialog-content/create-post-crop/zoom-handler/ZoomButton.tsx
@@ -1,16 +1,13 @@
 import React, { useState } from 'react'
 import { ReactCropperElement } from 'react-cropper'
 
-import {
-  AspectRatio,
-  optionsArray,
-} from '@/components/posts/create/ui/create-post-dialog-content/create-post-crop/create-post-cropp-options/create-post-cropp-options'
+import RangeSlider from '@/components/posts/create/ui/create-post-dialog-content/create-post-crop/range-slider/range-slider'
 import * as Dialog from '@radix-ui/react-dialog'
-import { Button, ExpandOutline, Label, MaximizeOutline } from '@robur_/ui-kit'
+import { Button, MaximizeOutline } from '@robur_/ui-kit'
 import clsx from 'clsx'
 
-import s from '@/components/posts/create/ui/create-post-dialog-content/create-post-crop/create-post-crop.module.scss'
-import RangeSlider from '@/components/posts/create/ui/create-post-dialog-content/create-post-crop/range-slider/range-slider'
+import s2 from '../create-post-crop.module.scss'
+import s from './create-post-cropp-zoom-button.module.scss'
 
 interface ExpandButtonProps {
   cropperRef: React.RefObject<ReactCropperElement>
@@ -19,39 +16,15 @@ interface ExpandButtonProps {
 export const ZoomButton = ({ cropperRef }: ExpandButtonProps) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false) // состояние диалога
 
-  // function handleOptionClick(name: string) {
-  //   const cropper = cropperRef.current?.cropper
-  //
-  //   if (cropper) {
-  //     switch (true) {
-  //       case name === AspectRatio.original:
-  //         cropper.reset()
-  //         break
-  //       case name === AspectRatio.ar1to1:
-  //         cropper.setAspectRatio(1)
-  //         break
-  //       case name === AspectRatio.ar4to5:
-  //         cropper.setAspectRatio(4 / 5)
-  //         break
-  //       case name === AspectRatio.ar16to9:
-  //         cropper.setAspectRatio(16 / 9)
-  //         break
-  //       default:
-  //         console.warn(`Unknown option clicked: ${name}`)
-  //     }
-  //   }
-  //   setIsDialogOpen(false)
-  // }
-
   return (
     <Dialog.Root onOpenChange={setIsDialogOpen} open={isDialogOpen}>
       <Dialog.Trigger asChild>
-        <Button className={clsx(s.createPostCroppBtn)} onClick={() => {}} variant={'secondary'}>
+        <Button className={clsx(s2.createPostCroppBtn)} onClick={() => {}} variant={'secondary'}>
           <MaximizeOutline />
         </Button>
       </Dialog.Trigger>
 
-      <Dialog.Portal>
+      <Dialog.Portal container={document.querySelector(`[class*="createPostCropBtnsBlock"]`)}>
         <Dialog.Overlay className={s.dialogOverlay} />
         <Dialog.Content className={s.dialogContent}>
           <RangeSlider />
