c01999a3885218ab4d424174d9eb96e1fdd6ee93
Shevchenko Andrey <demorest49de@gmail.com>
2024-10-29 20:55:20 +0300

    Changes range-slider.tsx

--- src/components/posts/create/ui/create-post-dialog-content/create-post-crop/range-slider/range-slider.tsx
+++ src/components/posts/create/ui/create-post-dialog-content/create-post-crop/range-slider/range-slider.tsx
@@ -4,41 +4,8 @@ import * as Slider from '@radix-ui/react-slider'
 
 import s from './range-slider.module.scss'
 
-// const styles = {
-//   sliderRange: {
-//     background: '#0070f3', // Цвет диапазона
-//     borderRadius: '3px',
-//     height: '100%',
-//   } as React.CSSProperties,
-//
-//   sliderRoot: {
-//     position: 'relative',
-//     userSelect: 'none',
-//   } as React.CSSProperties,
-//
-//   sliderThumb: {
-//     background: 'white', // Цвет ползунка
-//     border: '2px solid #0070f3', // Цвет рамки ползунка
-//     borderRadius: '50%',
-//     boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
-//     cursor: 'grab',
-//     height: '20px', // Высота ползунка
-//     width: '20px', // Ширина ползунка
-//   } as React.CSSProperties,
-//
-//   sliderTrack: {
-//     backgroundColor: '#e0e0e0', // Цвет трека
-//     borderRadius: '3px',
-//     height: '6px',
-//   } as React.CSSProperties,
-//
-//   sliderWrapper: {
-//     width: '300px', // Ширина ползунка
-//   } as React.CSSProperties,
-// }
-
 const RangeSlider = () => {
-  const [value, setValue] = useState<number[]>([0]) // Начальное значение 50%
+  const [value, setValue] = useState<number[]>([50])
 
   const handleValueChange = (newValue: number[]) => {
     setValue(newValue)
@@ -49,7 +16,6 @@ const RangeSlider = () => {
       <Slider.Root
         aria-label={'Percentage Slider'}
         className={s.sliderRoot}
-        defaultValue={[50]}
         max={100}
         min={0}
         onValueChange={handleValueChange}
