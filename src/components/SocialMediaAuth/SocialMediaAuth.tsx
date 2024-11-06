443c849d4123b2580ce01c15818c469b566f492e
Denis Krestov <rupo@list.ru>
2024-11-06 21:30:41 +0300

    [IN-144] fix oauth

--- src/components/SocialMediaAuth/SocialMediaAuth.tsx
+++ src/components/SocialMediaAuth/SocialMediaAuth.tsx
@@ -1,34 +1,15 @@
-import { useLazyGithubLoginQuery, useLazyGoogleLoginQuery } from '@/services/auth/authApi'
 import { GithubSvgrepoCom31, GoogleSvgrepoCom1 } from '@robur_/ui-kit'
 
 import s from './SocialMediaAuth.module.scss'
 
 export const SocialMediaAuth = () => {
-  const [googleLogin] = useLazyGoogleLoginQuery()
-  const [githubLogin] = useLazyGithubLoginQuery()
-
-  const googleLoginHandler = () => {
-    googleLogin()
-      .unwrap()
-      .catch(error => {
-        console.log(error)
-      })
-  }
-  const githubLoginHandler = () => {
-    githubLogin()
-      .unwrap()
-      .catch(error => {
-        console.log(error)
-      })
-  }
-
   return (
     <div className={s.container}>
-      <a className={s.btn} href={process.env.NEXT_PUBLIC_GOOGLE_AUTH_URL} type={'button'}>
-        <GoogleSvgrepoCom1 className={s.svg} onClick={googleLoginHandler} />
+      <a className={s.btn} href={'https://navaibe.ru/api/v1/auth/google'} type={'button'}>
+        <GoogleSvgrepoCom1 className={s.svg} />
       </a>
-      <a className={s.btn} href={process.env.NEXT_PUBLIC_GITHUB_AUTH_URL} type={'button'}>
-        <GithubSvgrepoCom31 className={s.svg} onClick={githubLoginHandler} />
+      <a className={s.btn} href={'https://navaibe.ru/api/v1/auth/github'} type={'button'}>
+        <GithubSvgrepoCom31 className={s.svg} />
       </a>
     </div>
   )
