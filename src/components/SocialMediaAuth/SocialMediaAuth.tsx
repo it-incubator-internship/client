475b3dd602c27ad04c4200663ad61e2db86f4a7a
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-11-07 12:22:36 +0400

    [IN-144] patch version

--- src/components/SocialMediaAuth/SocialMediaAuth.tsx
+++ src/components/SocialMediaAuth/SocialMediaAuth.tsx
@@ -5,10 +5,10 @@ import s from './SocialMediaAuth.module.scss'
 export const SocialMediaAuth = () => {
   return (
     <div className={s.container}>
-      <a className={s.btn} href={'https://navaibe.ru/api/v1/auth/google'} type={'button'}>
+      <a className={s.btn} href={'https://navaibe.ru/api/v1/auth/google'}>
         <GoogleSvgrepoCom1 className={s.svg} />
       </a>
-      <a className={s.btn} href={'https://navaibe.ru/api/v1/auth/github'} type={'button'}>
+      <a className={s.btn} href={'https://navaibe.ru/api/v1/auth/github'}>
         <GithubSvgrepoCom31 className={s.svg} />
       </a>
     </div>
